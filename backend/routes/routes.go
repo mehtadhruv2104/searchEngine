f15948c5c4145860693e827dbf31edf9c6bc020f
mehtadhruv2104 <mehtadhruv498@gmail.com>
2025-05-02 15:00:31 +0530

    Added CORS for local testing

--- backend/routes/routes.go
+++ backend/routes/routes.go
@@ -1,6 +1,9 @@
 package routes
 
 import (
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/mehtadhruv2104/searchEngine/backend/handler"
 )
@@ -8,6 +11,14 @@ import (
 
 func StartEngine(h *handler.Handler) (*gin.Engine){
 	router := gin.Default()
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"*"},
+		AllowMethods:     []string{"GET", "POST", "OPTIONS", "PUT", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length", "Content-Range", "X-Total-Count"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 	apiRoutes := router.Group("/api")
 	// set up middleware for further improvements 
 	//apiRoutes.Use(Middleware)
@@ -17,5 +28,5 @@ func StartEngine(h *handler.Handler) (*gin.Engine){
 
 
 
-	
+
 }
